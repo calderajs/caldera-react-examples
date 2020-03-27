97fc748c04160daac730c0c06129b42d7701c8cb
Yunyu Lin <mail@yunyul.in>
2020-03-26 22:14:03 -0600

    Add interactivity into new moo

--- src/twudder/index.tsx
+++ src/twudder/index.tsx
@@ -1,72 +1,86 @@
-import React, { useState } from 'react';
-import { renderCalderaApp, useLocation, useHistory, Head } from 'caldera';
+import React, { useState } from "react";
+import { renderCalderaApp, useLocation, useHistory, Head } from "caldera";
 import style from "./style";
 import NavBar from "./NavBar";
-import AccountPic from './AccountPic';
-
+import AccountPic from "./AccountPic";
 
 const MooBox = ({ children }: { children: React.ReactNode }) => {
-    return <div className="moo-box">
-        {children}
-    </div>
-}
+  return <div className="moo-box">{children}</div>;
+};
+
 const NewMoo = () => {
-    return <MooBox>
-        <div className="new-moo-wrapper">
-            <div className="new-moo-input-wrapper">
-                <AccountPic color="green" text="R" />
-                <textarea placeholder="Type your moo here..." className="moo-input"></textarea>
-            </div>
-            <div className="new-moo-submit-wrapper">
-                <span className="new-moo-char-count"> 0/140 </span>
-                <input type="button" className="moo-submit" value="Moo"></input>
-            </div>
+  const [value, setValue] = useState("");
+
+  return (
+    <MooBox>
+      <div className="new-moo-wrapper">
+        <div className="new-moo-input-wrapper">
+          <AccountPic color="green" text="R" />
+          <textarea
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Type your moo here..."
+            className="moo-input moo-textarea"
+            rows={5}
+            maxLength={140}
+          ></textarea>
         </div>
-    </MooBox >
-}
+        <div className="new-moo-submit-wrapper">
+          <span className="new-moo-char-count"> {value.length}/140 </span>
+          <input
+            type="button"
+            className="moo-submit"
+            value="Moo"
+            {...(value.length >= 140 && { disabled: true })}
+          ></input>
+        </div>
+      </div>
+    </MooBox>
+  );
+};
 
 const Moo = () => {
-    return <MooBox>
-        <div className="moo">
-            <div className="account">
-                <AccountPic color="red" text="R" />
-                <div className="account-name-wrapper">
-                    <div className="account-name">Rahul GS</div>
-                    <div className="account-id">@cho</div>
-                </div>
-            </div>
-
+  return (
+    <MooBox>
+      <div className="moo">
+        <div className="account">
+          <AccountPic color="red" text="R" />
+          <div className="account-name-wrapper">
+            <div className="account-name">Rahul GS</div>
+            <div className="account-id">@cho</div>
+          </div>
         </div>
+      </div>
     </MooBox>
-}
+  );
+};
 
 const Feed = () => {
-    return <div className="feed-outer">
-        <div className="feed-inner">
-            <NewMoo />
-            <Moo />
-        </div>
+  return (
+    <div className="feed-outer">
+      <div className="feed-inner">
+        <NewMoo />
+        <Moo />
+      </div>
     </div>
-}
+  );
+};
 
 const App = () => {
-
-    return (
-        <>
-            <Head>
-                <title>{`hi`}</title>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap"
-                    rel="stylesheet"
-                />
-                <style>{style}</style>
-            </Head>
-            <NavBar />
-            <Feed />
-
-        </>
-
-    );
+  return (
+    <>
+      <Head>
+        <title>{`hi`}</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <style>{style}</style>
+      </Head>
+      <NavBar />
+      <Feed />
+    </>
+  );
 };
 
 renderCalderaApp(<App />);
--- src/twudder/style.tsx
+++ src/twudder/style.tsx
@@ -7,27 +7,31 @@ const css = (input: TemplateStringsArray) => {
 };
 
 const style = css`
-
   *:focus {
-      outline: none;
+    outline: none;
   }
 
   body {
     height: 100%;
     margin: 0;
-    font-family: "Work Sans", sans-serif !important;
     font-size: 14px;
-    background: #F0F0F0;
+    background: #f0f0f0;
+  }
+
+  body,
+  input,
+  textarea {
+    font-family: "Work Sans", sans-serif;
   }
 
   .nav-outer {
     justify-content: space-between;
-    display: flex; 
+    display: flex;
     height: 64px;
     width: 100%;
     align-items: center;
-    background: #FFF;
-    box-shadow: 0 0 4px rgba(0,0,0,.15);
+    background: #fff;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.15);
   }
 
   .nav-account-outer {
@@ -53,13 +57,20 @@ const style = css`
   .moo-input {
     height: 42px;
     width: 400px;
-    border:none;
+    border: none;
     font-size: 16px;
+  }
+
+  .moo-input.moo-textarea {
+    height: auto;
+    resize: none;
+    width: 100%;
+    padding: 0;
   }
 
   .nav-search-wrapper {
     padding: 0 8px;
-    box-shadow: 0 2px 4px rgba(0,0,0,.15);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
     border-radius: 4px;
     /* border-style: solid; */
   }
@@ -78,12 +89,13 @@ const style = css`
     height: 42px;
     width: 42px;
     border-radius: 21px;
-    color: #FFF;
-    margin-right: 8px;
+    color: #fff;
+    margin-right: 12px;
     font-size: 24px;
     text-align: center;
     vertical-align: middle;
-    line-height: 42px;      
+    line-height: 42px;
+    flex-shrink: 0;
   }
 
   .account-name-wrapper {
@@ -97,7 +109,7 @@ const style = css`
   }
 
   .account-id {
-    color: #AAA;
+    color: #aaa;
   }
 
   .feed-outer {
@@ -113,43 +125,42 @@ const style = css`
   }
 
   .moo-box {
-    box-shadow: 0 2px 4px rgba(0,0,0,.15);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
     border-radius: 4px;
-    background: #FFF;
+    background: #fff;
     padding: 24px;
     margin: 16px 0;
-
-  
   }
 
   .new-moo-input-wrapper {
     display: flex;
+    margin-bottom: 12px;
   }
 
   .new-moo-submit-wrapper {
     display: flex;
-
   }
 
   .new-moo-wrapper {
     justify-content: space-between;
     display: flex;
     flex-direction: column;
-    /* height: 100%; */
-    min-height: 169px;
   }
 
   .moo-submit {
     border-radius: 4px;
-    background: #36A0EC;
-    color: #FFF;
+    background: #36a0ec;
+    color: #fff;
     padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  font-size: 16px;
-  border: none;
-  box-shadow: 0 2px 4px rgba(0,0,0,.15);
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    border: none;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+  }
 
+  .moo-submit:disabled {
+    background: #a0a0a0;
   }
 
   .new-moo-submit-wrapper {
@@ -160,9 +171,8 @@ const style = css`
   }
 
   .new-moo-char-count {
-    color: #AAA;
+    color: #aaa;
   }
-
 `;
 
 export default style;

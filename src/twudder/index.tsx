01ae146224c9764408bc60f90d2651fbc8465e2a
rahulgs12 <gsr1998@gmail.com>
2020-03-28 20:28:14 +0530

    refactor

--- src/twudder/index.tsx
+++ src/twudder/index.tsx
@@ -3,10 +3,8 @@ import { renderCalderaApp, useLocation, useHistory, Head } from "caldera";
 import style from "./style";
 import NavBar from "./NavBar";
 import AccountPic from "./AccountPic";
+import MooBox from "./MooBox";
 
-const MooBox = ({ children }: { children: React.ReactNode }) => {
-  return <div className="moo-box">{children}</div>;
-};
 
 const NewMoo = () => {
   const [value, setValue] = useState("");
@@ -66,46 +64,9 @@ const Feed = () => {
   );
 };
 
-const Login = () => {
-  const [isLogin, setIsLogin] = useState(false)
-
-  return <div className="login">
-    <MooBox>
-      <div className="login-inner">
-        <div className="toggle-wrapper">
-          <div className={`toggle ${isLogin ? "" : "active"}`} onClick={() => setIsLogin(false)}>
-            Sign up
-          </div>
-          <div className="spacer" ></div>
-          <div className={`toggle ${isLogin ? "active" : ""}`} onClick={() => setIsLogin(true)}>
-            Log in
-          </div>
-        </div>
-        <div className="input-wrapper">
-          <input placeholder="@username" className="moo-input "></input>
-        </div>
-        <div className="login-padding" />
-
-        <div className="input-wrapper">
-          <input placeholder="password" type="password" className="moo-input "></input>
-        </div>
-        {!isLogin ? <>
-          <div className="login-padding" />
-
-          <div className="input-wrapper">
-            <input placeholder="confirm password" type="password" className="moo-input "></input>
-          </div>
-        </> : <></>}
-        <div className="login-padding" />
-        <input type="button" value="Join Twudder" className="login-button"></input>
-
-      </div>
-    </MooBox>
-  </div>
-
-}
 
 const App = () => {
+  const [login, setLogin] = useState(null);
   return (
     <>
       <Head>
@@ -117,7 +78,6 @@ const App = () => {
         <style>{style}</style>
       </Head>
       <NavBar />
-      <Login />
       <Feed />
     </>
   );
